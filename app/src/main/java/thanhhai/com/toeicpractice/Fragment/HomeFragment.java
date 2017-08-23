0383e0449a039fc2339aaaeee8de870dce89d7c9
Thanh Hải <hai2571998@gmail.com>
2017-08-23 09:37:26 +0700

    Update 23/8/2017

--- app/src/main/java/thanhhai/com/toeicpractice/Fragment/HomeFragment.java
+++ app/src/main/java/thanhhai/com/toeicpractice/Fragment/HomeFragment.java
@@ -1,31 +1,104 @@
 package thanhhai.com.toeicpractice.Fragment;
 
 
-import android.content.res.Resources;
-import android.graphics.Rect;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
-import thanhhai.com.toeicpractice.HomeModel.Home;
+import java.util.HashMap;
+import java.util.Random;
+
 import thanhhai.com.toeicpractice.HomeModel.HomeAdapter;
+import thanhhai.com.toeicpractice.HomeModel.QuestionActivity;
+import thanhhai.com.toeicpractice.HomeModel.TestVocabularyActivity;
+import thanhhai.com.toeicpractice.HomeModel.VideoYoutubeActivity;
 import thanhhai.com.toeicpractice.R;
+import thanhhai.com.toeicpractice.RoomChat.SplashLoginActivity;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
-    private RecyclerView recyclerView;
-    private HomeAdapter adapter;
-    private List<Home> homeList;
+    private SliderLayout imageSlider;
+    private int max = 12;
+
+    private String webUrl = "https://toeicpractice.000webhostapp.com/image/";
+    private String[] url1 = {
+            "abide_by", "agreement", "assurance", "cancellation", "determine",
+            "engage", "establish", "obligate", "party", "provision", "resolve",
+            "specific"
+    };
+
+    private String[] url2 = {
+            "attract", "compare", "competition", "consume", "convince", "currently",
+            "fad", "inspiration", "market", "persuasion", "productive", "satisfaction"
+    };
+
+    private String[] url3 = {
+            "characteristic", "consequence", "consider", "cover", "expiration",
+            "frequently", "imply", "promise", "protect", "reputation", "require",
+            "variety"
+    };
+
+    private String[] url4 = {
+            "address", "avoid", "demonstrate", "develop", "evaluate", "gather",
+            "offer", "primarily", "risk", "strategy", "strong", "substitution"
+    };
+
+    private String[] url5 = {
+            "accommodate", "arrangement", "association", "attend", "get_in_touch",
+            "hold", "location", "overcrowded", "register", "select", "session", "take_part_in"
+    };
+
+    private String[] mean1 = {
+            "tuân theo", "hợp đồng", "sự đảm bảo", "sự hủy bỏ", "xác định, định rõ", "tham gia",
+            "thành lập", "bắt buộc", "bữa tiệc", "điều khoản", "giải quyết", "định rỏ"
+    };
+
+    private String[] mean2 = {
+            "thu hút, hấp dẫn", "so sánh", "cạnh tranh", "tiêu thụ, hấp thụ", "thuyết phục",
+            "hiện tại", "mốt nhất thời", "cảm hứng", "thị trường", "sự thuyết phục", "có năng suất",
+            "sự thỏa mãn"
+    };
+
+    private String[] mean3 = {
+            "đặc điểm", "hậu quả, kết quả", "xem xét", "bảo vệ", "hết hạn", "thường xuyên",
+            "ngụ ý", "hứa hẹn", "bảo vệ", "danh tiếng", "yêu cầu", "nhiều loại khác nhau"
+    };
+
+    private String[] mean4 = {
+            "diển thuyết", "tránh", "chứng minh", "phát triển", "đánh giá", "tập hợp, thu thập",
+            "đề nghị", "chủ yếu, chính", "rủi ro", "chiến lược", "mạnh mẽ", "thay thế"
+    };
+
+    private String[] mean5 = {
+            "cung cấp", "sắp xếp", "hiệp hội", "tham dự", "giữ liên lạc", "tổ chức, chủ trì",
+            "vị trí, địa điểm", "quá đông", "đăng ký", "chọn", "kỳ họp, phiên họp", "tham gia vào"
+    };
+
+    private String dinhdangfile = ".jpg";
+
+    GridView gvMenu;
+    private String[] tenLogo = {
+            "Question Online", "Video Toeic", "Room Chat", "Test Vocabulary"
+    };
+
+    int[] imgLogo = {
+            R.drawable.question,
+            R.drawable.videotoiec,
+            R.drawable.roomchat,
+            R.drawable.test_vocabulary
+    };
 
     public HomeFragment() {
 
@@ -40,78 +113,89 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        recyclerView = (RecyclerView) getActivity().findViewById(R.id.recycler_view);
-        homeList = new ArrayList<>();
-        adapter = new HomeAdapter(getContext(), homeList);
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext(), 2);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
-        prepareHome();
-    }
+        imageSlider = (SliderLayout) getActivity().findViewById(R.id.sliderMenu);
+        Random random = new Random();
+        int index  = random.nextInt(max);
+        HashMap<String, String> url_maps = new HashMap<String, String>();
+        url_maps.put(url1[index]+": "+mean1[index], webUrl+url1[index]+dinhdangfile);
+        url_maps.put(url2[index]+": "+mean2[index], webUrl+url2[index]+dinhdangfile);
+        url_maps.put(url3[index]+": "+mean3[index], webUrl+url3[index]+dinhdangfile);
+        url_maps.put(url4[index]+": "+mean4[index], webUrl+url4[index]+dinhdangfile);
+        url_maps.put(url5[index]+": "+mean5[index], webUrl+url5[index]+dinhdangfile);
 
-    private void prepareHome() {
-        int[] covers = new int[]{
-                R.drawable.question,
-                R.drawable.videotoiec,
-                R.drawable.test_vocabulary,
-                R.drawable.roomchat};
+        for (String name : url_maps.keySet()) {
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
 
-        Home a = new Home("Question Online", covers[0]);
-        homeList.add(a);
-
-        a = new Home("Video Toeic", covers[1]);
-        homeList.add(a);
-
-        a = new Home("Vocabulary Test", covers[2]);
-        homeList.add(a);
-
-        a = new Home("Room Chat English", covers[3]);
-        homeList.add(a);
-
-        adapter.notifyDataSetChanged();
-    }
-
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle().putString("extra", name);
+            imageSlider.addSlider(textSliderView);
         }
+        imageSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        imageSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        imageSlider.setCustomAnimation(new DescriptionAnimation());
+        imageSlider.setDuration(3000);
+        imageSlider.addOnPageChangeListener(this);
 
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
+        gvMenu = (GridView) getActivity().findViewById(R.id.gvMenu);
+        HomeAdapter homeAdapter = new HomeAdapter(getContext(), tenLogo, imgLogo);
+        gvMenu.setAdapter(homeAdapter);
+        gvMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getContext(), QuestionActivity.class);
+                        getActivity().startActivity(intent);
+                        break;
+                    case 1:
+                        Intent intent1 = new Intent(getContext(), VideoYoutubeActivity.class);
+                        getActivity().startActivity(intent1);
+                        break;
+                    case 2:
+                        Intent intent2 = new Intent(getContext(), SplashLoginActivity.class);
+                        getActivity().startActivity(intent2);
+                        break;
+                    case 3:
+                        Intent intent3 = new Intent(getContext(), TestVocabularyActivity.class);
+                        getActivity().startActivity(intent3);
+                        break;
+                    default:
+                        break;
                 }
             }
-        }
+        });
     }
 
-    /**
-     * Converting dp to pixel
-     */
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    @Override
+    public void onStop() {
+        imageSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
--- app/src/main/java/thanhhai/com/toeicpractice/HomeModel/HomeAdapter.java
+++ app/src/main/java/thanhhai/com/toeicpractice/HomeModel/HomeAdapter.java
@@ -1,81 +1,52 @@
 package thanhhai.com.toeicpractice.HomeModel;
 
 import android.content.Context;
-import android.content.Intent;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-
-import java.util.List;
-
 import thanhhai.com.toeicpractice.R;
-import thanhhai.com.toeicpractice.RoomChat.SplashLoginActivity;
 
-public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
-    private Context mContext;
-    private List<Home> homeList;
+public class HomeAdapter extends BaseAdapter{
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView title;
-        public ImageView thumbnail;
+    private Context context;
+    private String[] tenLogo;
+    private int[] imgLogo;
 
-        public MyViewHolder(View view) {
-            super(view);
-            title = (TextView) view.findViewById(R.id.title);
-            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-        }
-    }
-
-
-    public HomeAdapter(Context mContext, List<Home> homeList) {
-        this.mContext = mContext;
-        this.homeList = homeList;
+    public HomeAdapter(Context context, String[] tenLogo, int[] imgLogo) {
+        this.context = context;
+        this.tenLogo = tenLogo;
+        this.imgLogo = imgLogo;
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.home_cardview, parent, false);
-        return new MyViewHolder(itemView);
+    public int getCount() {
+        return tenLogo.length;
     }
 
     @Override
-    public void onBindViewHolder(final MyViewHolder holder, final int position) {
-        Home home = homeList.get(position);
-        holder.title.setText(home.getName());
-
-        Glide.with(mContext).load(home.getThumbnail()).into(holder.thumbnail);
-        holder.thumbnail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (position) {
-                    case 0:
-                        Intent intent = new Intent(view.getContext(), QuestionActivity.class);
-                        view.getContext().startActivity(intent);
-                        break;
-                    case 1:
-                        Intent intent1 = new Intent(view.getContext(), VideoYoutubeActivity.class);
-                        view.getContext().startActivity(intent1);
-                        break;
-                    case 2:
-                        Intent intent2 = new Intent(view.getContext(), TestVocabularyActivity.class);
-                        view.getContext().startActivity(intent2);
-                        break;
-                    case 3:
-                        Intent intent3 = new Intent(view.getContext(), SplashLoginActivity.class);
-                        view.getContext().startActivity(intent3);
-                }
-            }
-        });
+    public Object getItem(int i) {
+        return null;
     }
 
     @Override
-    public int getItemCount() {
-        return homeList.size();
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.home_item, null);
+        TextView txtMenuHome = (TextView) view.findViewById(R.id.txtMenuHome);
+        ImageView imgMenuHome = (ImageView) view.findViewById(R.id.imgMenuHome);
+
+        txtMenuHome.setText(tenLogo[position]);
+        imgMenuHome.setImageResource(imgLogo[position]);
+
+        return view;
     }
 }
